010cadee9970d6b6de2098f33a0b1abb1159e4d1
Carneyval <pramudiar101@gmail.com>
2026-06-16 22:51:10 +0700

    remove vercel build files

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-Di1-IHoC.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/Keperluan kerja/PortoFix/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-DWFbjJCv.js"] }, "/": { filePath: "D:/Keperluan kerja/PortoFix/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-6KywUzss.js"] } }, clientEntry: "/assets/index-DWFbjJCv.js" });
-export {
-  tsrStartManifest
-};
